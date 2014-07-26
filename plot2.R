313104a686b2fd5b6837e20f6bdabce6217af7a2
MejiUlises <jonhylargo@gmail.com>
2014-07-25 19:45:39 -0500

    Minor Changes

--- plot2.R
+++ plot2.R
@@ -17,5 +17,5 @@ TidySet <- dcast(meltData, year  ~ variable, sum)
 
 #We make the plot and save it to a file
 png(file="plot2.png", width=960, height=960)
-with(TidySet, barplot(Emissions, main=expression('Total Emission of PM'[2.5]*' by Year'),xlab="Year", ylab=expression("Total amount of PM"[2.5]*" Produced"), names.arg=TidySet$year, col="steelblue"))
+with(TidySet, barplot(Emissions, main=expression('Total Emission of PM'[2.5]*' by Year of Baltimore City'),xlab="Year", ylab=expression("Total amount of PM"[2.5]*" Produced"), names.arg=TidySet$year, col="steelblue"))
 dev.off()
---
+++ plot3.R
@@ -0,0 +1,25 @@
+#We load the library so we can reshape the Data
+library(reshape2) 
+library(ggplot2)
+
+#We load the filos into R
+NEI <- readRDS("../summarySCC_PM25.rds")
+SCC <- readRDS("../Source_Classification_Code.rds")
+
+#We cast the year and SCC to factors
+#NEI$year <- as.factor(NEI$year)
+NEI$SCC <- as.factor(NEI$SCC)
+NEI$type <- as.factor(NEI$type)
+
+NEI <- NEI[NEI$fips=="24510",]
+
+meltData <- melt(NEI[NEI$fips=="24510",], id.vars=c("year","type"), measure.vars=c("Emissions"))
+
+TidyData <- dcast(meltData, year + type ~ variable, sum)
+
+png(file="plot3.png", width=1050, height=960)
+plot <- ggplot(TidyData, aes(year, Emissions, color=type)) + geom_point()
+plot <- plot + facet_grid(.~type) + geom_smooth(method="lm")
+plot <- plot + labs(title=expression("Emissions of PM"[2.5]*" By Year by Type for Baltimore City"), x="Years", y=expression("Total Emission of PM"[2.5]))
+plot
+dev.off()
