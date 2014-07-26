968aa7d8e212ffbb7e0e62934936f980c154b042
MejiUlises <jonhylargo@gmail.com>
2014-07-25 19:19:21 -0500

    Added comments and some annotations

--- plot1.R
+++ plot1.R
@@ -1,13 +1,21 @@
-library(reshape2)
+#We load the library so we can reshape the Data
+library(reshape2) 
+
+#We load the filos into R
 NEI <- readRDS("../summarySCC_PM25.rds")
 SCC <- readRDS("../Source_Classification_Code.rds")
 
+#We cast the year and SCC to factors
 NEI$year <- as.factor(NEI$year)
 NEI$SCC <- as.factor(NEI$SCC)
 
-#NEISCC <- merge(NEI, SCC, by.x="SCC", by.y="SCC")
-
+#We melt the data by year and SCC
 meltData <- melt(NEI, id.vars=c("year","SCC"), measure.vars=c("Emissions"))
+
+#We reshape the data by year adding all the values for each year.
 TidySet <- dcast(meltData, year ~ variable, sum)
 
-with(TidySet, barplot(Emissions, main=expression('Total Emission of PM'[2.5]*' by Year'),xlab="Year", ylab=expression("Total amount of PM"[2.5]*" Produced"), names.arg=TidySet$year, col="steelblue"))
+#We make the plot and save it to a file
+png(file="plot1.png", width=960, height=960)
+with(TidySet, barplot(Emissions/1000, main=expression('Total Emission of PM'[2.5]*' by Year'),xlab="Year", ylab=expression("Total amount of PM"[2.5]*" Produced (1K)"), names.arg=TidySet$year, col="steelblue"))
+dev.off()
